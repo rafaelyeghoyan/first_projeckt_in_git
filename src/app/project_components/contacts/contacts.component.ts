9481abd72268ae9732a55c94feaceb4a2f8b5cc4
rafaelyeghoyan <rvvebcss@gmail.com>
2022-05-31 16:13:24 +0400

    rafael:formery bacvox darcrel em responsive

--- src/app/project_components/contacts/contacts.component.ts
+++ src/app/project_components/contacts/contacts.component.ts
@@ -8,18 +8,15 @@ import {FormBuilder, Validators} from "@angular/forms";
 })
 export class ContactsComponent implements OnInit {
   form = this.fb.group({
-    tellnmber:['',[Validators.required,Validators.pattern(/^(0-9)/gm)]],
-    username:['',[Validators.required, Validators.pattern(/^[a-z][^0-9]$/gm)]],
-    email:['',[Validators.required,Validators.pattern(/^[a-z]@[a-z]{1,7}\.[a-z]{2,4}$/gmi)]],
-    sms: ['',[Validators.required,]],
-    secur:['',[Validators.required,]]
+    tellnmber:[''],
+    username:[''],
+    email:[''],
+    sms: [''],
+    secur:['']
   })
   constructor(public fb:FormBuilder,public  el:ElementRef) { }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  submit(){}
-  save(){}
   openForm(){
     this.el.nativeElement.querySelector('.hidden_block').style.display= 'block';
     this.el.nativeElement.querySelector('.form_block').style.display= 'block';
@@ -28,6 +25,8 @@ export class ContactsComponent implements OnInit {
     this.el.nativeElement.querySelector('.form_block').style.display= 'none';
     this.el.nativeElement.querySelector('.thanks_block').style.display = 'block';
     this.form.reset()
+    console.log(this.form.invalid);
+    console.log(this.form.valid);
   }
   goPrevButton(){
     this.el.nativeElement.querySelector('.hidden_block').style.display= 'none';
