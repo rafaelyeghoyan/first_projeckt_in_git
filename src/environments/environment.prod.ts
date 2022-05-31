c3f28294c87a31a84156f6efe586d6bf1261c864
rafaelyeghoyan <rvvebcss@gmail.com>
2022-05-31 18:58:05 +0400

    rafael:sertificati ejy ngForov em grel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
-const url = '';
+const url = 'http://localhost:4200';
 export const environment = {
   production: true,
   http: {
     homeData:{
       homeRoutUrl: url + '/home'
     },
+    certificateJsonUrl:'http://localhost:3000/sertificat'
   }
 };
